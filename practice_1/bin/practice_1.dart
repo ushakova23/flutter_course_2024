12bbb4da425526c322d07dd7624bcf2b7cd55da2
Alexandra Ushakova <113995972+ushakova23@users.noreply.github.com>
2024-10-20 20:27:29 +0300

    Update practice_1.dart

--- practice_1/bin/practice_1.dart
+++ practice_1/bin/practice_1.dart
@@ -1,14 +1,16 @@
-import 'package:practice_1/features/core/data/debug/weather_repository_debug.dart';
 import 'package:practice_1/features/core/data/osm/osm_api.dart';
-import 'package:practice_1/features/core/data/osm/weather_repository_osm.dart';
+import 'package:practice_1/features/core/data/weatherapi/wapi_api.dart';
+import 'package:practice_1/features/core/data/weatherapi/weather_repository_wapi.dart';
 import 'package:practice_1/features/core/presentation/app.dart';
+import 'package:practice_1/features/core/data/osm/weather_repository_osm.dart';
 
 const String version = '0.0.1';
-const String url = 'https://api.openweathermap.org';
-const String apiKey = 'f11a8d09666e4acbd56e3ecc1ccbe31b';
+const String url_osm = 'https://api.openweathermap.org';
+const String url_wapi = 'http://api.weatherapi.com/v1';
+const String apiKey = 'key';
 
 void main(List<String> arguments) {
-  var app = App(WeatherRepositoryDebug());
+  var app = App(WeatherRepositoryOSM(OSMApi(url_osm, apiKey)));
 
   app.run();
 }
